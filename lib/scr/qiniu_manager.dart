1c28045eb19ddf2a890f05d24384a0070243ac95
luy <luy7788@163.com>
2022-11-11 18:36:15 +0800

    注释

--- lib/scr/qiniu_manager.dart
+++ lib/scr/qiniu_manager.dart
@@ -12,8 +12,12 @@ class QiNiuManager {
   factory QiNiuManager() => _instance;
 
   ///上传文件
-  ///uploadFile 删除文件
-  ///
+  ///uploadFile 上传文件
+  ///token 接口返回七牛token
+  ///urlPrefix 返回地址需要拼接的前缀
+  ///progress 上传进度
+  ///sucCallback 成功回调
+  ///failCallback 失败回调
   void uploadFile(
     File uploadFile,
     String token,
